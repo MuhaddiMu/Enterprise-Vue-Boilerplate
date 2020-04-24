f2ca30a9befa5b765f2e08dd886ca193383a8693
Muhaddis <muhaddisshah@gmail.com>
2020-04-25 02:52:07 +0500

    :boom: Improve Application

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-        <h1>Profile Information</h1>
-        <span>Name: </span><span>{{name}}</span>
-        <span>Role: </span><span>{{role}}</span>
-    </div>
+  <div>
+    <h1>Profile Information</h1>
+    <span>Name: </span><span>{{ name }}</span> <span>Role: </span
+    ><span>{{ role }}</span>
+  </div>
 </template>
 <script>
 export default {
-    data () {
-        return {
-            name: "Jose Ponce",
-            role: "Software Engineer"
-        }
-    } 
-}
+  data() {
+    return {
+      name: "Jose Ponce",
+      role: "Software Engineer"
+    };
+  }
+};
 </script>
